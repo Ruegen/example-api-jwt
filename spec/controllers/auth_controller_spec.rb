a722f60e4041bea5eb7346b9c27724ec4245e9a9
ruegen <ruegen@icloud.com>
2018-07-09 13:55:26 +1000

    add tests for auth

--- spec/controllers/auth_controller_spec.rb
+++ spec/controllers/auth_controller_spec.rb
@@ -1,19 +1,56 @@
 require 'rails_helper'
+require 'jwt'
 
 RSpec.describe AuthController, type: :controller do
+  
+  let(:password) {'12345678'}
+  let(:secret) { ENV.fetch('JWT_SECRET')}
 
-  describe "GET #register" do
-    it "returns http success" do
-      get :register
-      expect(response).to have_http_status(:success)
+  describe "#login " do 
+
+    it "#does not create jwt cookie" do 
+    
+      user = build(:user)
+
+      post :login, params: {
+        auth: {
+          email: user.email, 
+          password: password
+        }
+      }
+      access_token = response.cookies['access_token']
+
+      expect(response).to have_http_status(422)
+      expect(access_token).to eq(nil)
     end
+
+
+    it "creates jwt in cookie" do
+
+      user = create(:user)
+
+      post :login, params: {
+        auth: {
+          email: user.email, 
+          password: password
+        }
+      }
+      
+      payload = {
+        email: user.email
+      }
+      
+      token = JWT.encode payload, secret
+
+      access_token = response.cookies['access_token']
+
+      expect(response).to be_successful
+      expect(access_token).to eq(token)
+      
+      
+    end
+
   end
 
-  describe "GET #login" do
-    it "returns http success" do
-      get :login
-      expect(response).to have_http_status(:success)
-    end
-  end
 
 end
