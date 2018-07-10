b24ac5f678f6dc6b3d4ea5d9f3f9ff5905495ef1
ruegen <ruegen@icloud.com>
2018-07-10 10:39:23 +1000

    refactor jclearwt method to module

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,5 +1,6 @@
 class AuthController < ApplicationController
-
+  include JsonWebTokens
+  
   def register
     user = User.new(login_params)
 
@@ -36,20 +37,4 @@ class AuthController < ApplicationController
     params.require(:auth).permit(:email, :password)
   end
 
-  def jwt_cookie user
-    secret = ENV.fetch('JWT_SECRET')
-
-    payload = {
-      email: user.email
-    }
-
-    token = JWT.encode(payload, secret)
-
-    return {
-      value: token,
-      expires: 30.minutes.from_now,
-      domain: 'localhost:3000',
-      httponly: true
-    }
-  end
 end
---
+++ app/controllers/concerns/json_web_tokens.rb
@@ -0,0 +1,19 @@
+module JsonWebTokens
+
+    def jwt_cookie user
+        secret = ENV.fetch('JWT_SECRET')
+
+        payload = {
+            email: user.email
+        }
+
+        token = JWT.encode(payload, secret)
+
+        return {
+            value: token,
+            expires: 30.minutes.from_now,
+            domain: 'localhost:3000',
+            httponly: true
+        }
+    end
+end
