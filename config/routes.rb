faef71487254b390d26341e0f74153d3b5536eeb
ruegen <ruegen@icloud.com>
2018-07-12 12:41:24 +1000

    change line alteration with response format

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  namespace :auth do
-    post :login, :register, defaults: { format: :json } 
+  namespace :auth, defaults: { format: :json } do
+    post :login, :register 
   end
 
 end
