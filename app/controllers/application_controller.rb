b0524709b4d6b90f61eb8ef672590e34836dbe29
ruegen <ruegen@icloud.com>
2018-07-12 11:12:25 +1000

    change comment out error handler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::API
 
-    rescue_from Exception do |exception|
-        render json: exception, status: 500
-    end
+    # rescue_from Exception do |exception|
+    #     render json: exception, status: 500
+    # end
 end
